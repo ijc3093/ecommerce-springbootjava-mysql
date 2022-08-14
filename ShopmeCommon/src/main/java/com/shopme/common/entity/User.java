a55037570cf488c32b27920f1a9afed8652ea23d
ijc3093 <49662848+ijc3093@users.noreply.github.com>
2022-08-14 00:05:56 -0400

    make it better

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -13,7 +13,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-import com.shopme.common.Constants;
+//import com.shopme.common.Constants;
 
 @Entity
 @Table(name = "users")
@@ -124,7 +124,7 @@ public class User extends IdBasedEntity {
 	public String getPhotosImagePath() {
 		if (id == null || photos == null) return "/images/default-user.png";
 		
-		return Constants.S3_BASE_URI + "/user-photos/" + this.id + "/" + this.photos;
+		return "/user-photos/" + this.id + "/" + this.photos;
 	}
 	
 	@Transient

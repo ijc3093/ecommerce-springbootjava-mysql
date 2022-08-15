36d7377c3f78ac8bdaaa02a6e170e8e42f763dd2
ijc3093 <49662848+ijc3093@users.noreply.github.com>
2022-08-15 00:10:40 -0400

    Replaced with new files

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f7/c0f2354e3b1c001d1e3becc572f46dc8
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFrontendApplication.class, args);
+	}
+
+}
---
+++ EcommerceProject/EcommerceWebParent/EcommerceFrontend/src/main/java/com/ecommerce/site/EcommerceFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFrontendApplication.class, args);
+	}
+
+}
